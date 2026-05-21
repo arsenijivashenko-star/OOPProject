5d013f82604a55fbb5e8aa576757cc66cb8f2580
arsenyivashenko-svg <arsenyivashenko@gmail.com>
2026-05-21 21:39:03 +0300

    Додано клас інвестора

---
+++ Investor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StartupPlatform
+{
+    public class Investor
+    {
+        public string Name { get; set; }
+        public double Budget { get; set; }
+
+        public Investor(string name, double budget)
+        {
+            Name = name;
+            Budget = budget;
+        }
+
+        public void Invest(Startup startup, double amount)
+        {
+            if (Budget >= amount)
+            {
+                Budget -= amount;
+                startup.CurrentFunding += amount;
+                Console.WriteLine($"{Name} інвестував {amount}$ у {startup.Name}");
+            }
+        }
+    }
+}
